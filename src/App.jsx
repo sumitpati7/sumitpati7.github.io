a0332c1bbb593d6c8a3d2d0b701d490e6b55d78f
Sumit Pati <sumitpati7@gmail.com>
2026-03-12 21:31:41 +0545

    feat: default colors using theme

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="min-h-screen bg-primaryBg">
+    <main className="min-h-screen bg-primary-bg">
       <Navbar/>
     </main>
   );
