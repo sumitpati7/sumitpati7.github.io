4a1c5ac4bed0a81b3057fb97d653946b2fadc8b6
Sumit Pati <sumitpati7@gmail.com>
2026-03-12 21:25:16 +0545

    fix: default color changes for navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const Navbar = () => {
   return (
-    <nav className='bg-green-800'>
-      Check
+    <nav className="bg-green-800 h-16 w-full">
+      <section className="container mx-auto flex justify-between items-center h-full">
+        <div>Check</div>
+      </section>
     </nav>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
 import { Navbar } from "../components/Navbar";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-primaryBg">
       <Navbar/>
     </main>
   );
