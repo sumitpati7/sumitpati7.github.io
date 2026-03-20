424aca49cbf0d9f8e3bc9e103b95998991e4e2d2
Sumit Pati <156872571+sumitpati7@users.noreply.github.com>
2026-03-20 20:29:10 +0545

    CI: fix deploy script and manual deploy (#11)
    * feat: manual deploy script
    
    * test manual deploy
    
    * ci: update GitHub Actions to latest versions and add Dependabot
    
    * ci: message discord after success

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <main className="min-h-screen bg-primary-bg">
       <Navbar/>
+      <div className="min-h-[calc(100vh-74px)] flex items-center justify-center text-primary">SUMITPATI</div>
     </main>
   );
 }
